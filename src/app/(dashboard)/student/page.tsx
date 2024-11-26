d8419d76160832f8da72e7d8d0a5ca68fd0a3e98
kncode86 <kulcsar.norbert.86@gmail.com>
2024-11-26 10:29:16 +0000

    feat: create Student Dashboard design

--- src/app/(dashboard)/student/page.tsx
+++ src/app/(dashboard)/student/page.tsx
@@ -1,6 +1,22 @@
+import Announcements from "@/components/Announcements"
+import EventCalendar from "@/components/EventCalendar"
+
 const StudentPage = () => {
     return (
-        <div className="">StudentPage</div>
+        <div className="p-4 gap-4 flex flex-col xl:flex-row">
+            {/*LEFT*/}
+            <div className="w-full xl:w-2/3">
+                <div className="h-full bg-white p-4 rounded-md">
+                    <h1 className="text-xl font-semibold">Schedule (4A)</h1>
+                </div>
+            </div>
+
+            {/*RIGHT*/}
+            <div className="w-full xl:w-1/3 flex flex-col gap-8">
+                <EventCalendar />
+                <Announcements />
+            </div>
+        </div>
     )
 }
 
