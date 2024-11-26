e94d739f68eb7a1e667346203cfe72036990b886
kncode86 <kulcsar.norbert.86@gmail.com>
2024-11-26 22:26:47 +0000

    feat: create responsive reusable List Table

--- src/app/(dashboard)/list/teachers/page.tsx
+++ src/app/(dashboard)/list/teachers/page.tsx
@@ -1,8 +1,94 @@
 import Pagination from "@/components/Pagination";
+import Table from "@/components/Table";
 import TableSearch from "@/components/TableSearch";
+import { role, teachersData } from "@/lib/data";
 import Image from "next/image";
+import Link from "next/link";
+
+type Teacher = {
+    id:string;
+    teacherId:string;
+    name:string;
+    email?:string;
+    photo:string;
+    phone:string;
+    subjects:string[];
+    classes:string[];
+    address:string;
+} 
+
+const columns = [
+    {
+        header: "Info",
+        accessor: "info",
+    },
+    {
+        header: "Teacher ID",
+        accessor: "teacherId",
+        className:"hidden md:table-cell",
+    },
+    {
+        header: "Subjects",
+        accessor: "subjects",
+        className:"hidden md:table-cell",
+    },
+    {
+        header: "Classes",
+        accessor: "classes",
+        className:"hidden md:table-cell",
+    },
+    {
+        header: "Phone",
+        accessor: "phone",
+        className:"hidden lg:table-cell",
+    },
+    {
+        header: "Address",
+        accessor: "address",
+        className:"hidden lg:table-cell",
+    },
+    {
+        header: "Actions",
+        accessor: "action",
+    }
+];
 
 const TeacherListPage = () => {
+    const renderRow = (item:Teacher) => (
+        <tr key={item.id} className="border-b border-gray-200 even:bg-slate-50 text-sm hover:bg-customPurpleLight">
+            <td className="flex items-center gap-4 p-4">
+                <Image
+                    src={item.photo}
+                    alt=""
+                    width={40}
+                    height={40}
+                    className="md:hidden xl:block w-10 h-10 rounded-full object-cover"
+                />
+                <div className="flex flex-col">
+                    <h3 className="font-semibold">{item.name}</h3>
+                    <p className="text-xs text-gray-500">{item?.email}</p>
+                </div>
+            </td>
+            <td className="hidden md:table-cell">{item.teacherId}</td>
+            <td className="hidden md:table-cell">{item.subjects.join(",")}</td>
+            <td className="hidden md:table-cell">{item.classes.join(",")}</td>
+            <td className="hidden md:table-cell">{item.phone}</td>
+            <td className="hidden md:table-cell">{item.address}</td>
+            <td>
+                <div className="gap-2 flex items-center">
+                    <Link href={`/list/teachers/${item.id}`}>
+                        <button className="w-7 h-7 flex items-center justify-center rounded-full bg-customSky">
+                            <Image src="/view.png" alt="" width={16} height={16} />
+                        </button>
+                    </Link>
+                    {role === "admin" && <button className="w-7 h-7 flex items-center justify-center rounded-full bg-customPurple">
+                        <Image src="/delete.png" alt="" width={16} height={16} />
+                    </button>}
+                </div>
+            </td>
+        </tr>
+    )
+
     return (
         <div className="bg-white p-4 rounded-md flex-1 m-4 mt-0">
             {/*TOP*/}
@@ -25,7 +111,7 @@ const TeacherListPage = () => {
             </div>
 
             {/*LIST*/}
-            <div className=""></div>
+            <Table columns={columns} renderRow={renderRow} data={teachersData} />
 
             {/*PAGINATION*/}
             <Pagination />
