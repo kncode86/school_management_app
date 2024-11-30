5a5f2987b53831d88462352458f508f2720c03b6
kncode86 <kulcsar.norbert.86@gmail.com>
2024-11-30 22:03:14 +0000

    feat: create React-Hook-Form with Zod Validation

--- src/components/FormModal.tsx
+++ src/components/FormModal.tsx
@@ -2,6 +2,7 @@
 
 import Image from "next/image";
 import { useState } from "react";
+import TeacherForm from "./forms/TeacherForm";
 
 const FormModal = ({table, type, data, id} : {
     table: 
@@ -32,35 +33,39 @@ const FormModal = ({table, type, data, id} : {
     const [open, setOpen] = useState(false);
 
     const Form = () => {
-        return type === 'delete' && id ? 
+        return type === 'delete' && id ? (
             <form action="" className="p-4 flex flex-col gap-4">
                 <span className="text-center font-medium">All data will be lost. Are you sure you want to delete this {table}?</span>
                 <button className="bg-red-700 text-white px-4 py-2 rounded-md border-none w-max self-center">Delete</button>
             </form> 
-            : "create or update form"
+        ): ( 
+            <TeacherForm type="create" /> 
+        )
     }
 
-    return <>
-        <button 
-            className={`${size} flex items-center justify-center rounded-full ${bgColor}`}
-            onClick={() => setOpen(true)}
-        >
-            <Image src={`/${type}.png`} alt="" width={16} height={16} />
-        </button>
-        { open && 
-            <div 
-                className="w-screen h-screen absolute left-0 top-0 bg-black bg-opacity-60 z-50
-                flex items-center justify-center"
+    return (
+        <>
+            <button 
+                className={`${size} flex items-center justify-center rounded-full ${bgColor}`}
+                onClick={() => setOpen(true)}
             >
-                <div className="bg-white p-4 rounded-md relative w-[90%] md:w-[70%] lg:w-[60%] xl:w-[50%] 2xl:w-[40%]">
-                    <Form />
-                    <div className="absolute top-4 right-4 cursor-pointer" onClick={() => setOpen(false)}>
-                        <Image src="/close.png" alt="" width={14} height={14} />
-                    </div>
-                </div>            
-            </div> 
-        }
-    </>
+                <Image src={`/${type}.png`} alt="" width={16} height={16} />
+            </button>
+            { open && (
+                <div 
+                    className="w-screen h-screen absolute left-0 top-0 bg-black bg-opacity-60 z-50
+                    flex items-center justify-center"
+                >
+                    <div className="bg-white p-4 rounded-md relative w-[90%] md:w-[70%] lg:w-[60%] xl:w-[50%] 2xl:w-[40%]">
+                        <Form />
+                        <div className="absolute top-4 right-4 cursor-pointer" onClick={() => setOpen(false)}>
+                            <Image src="/close.png" alt="" width={14} height={14} />
+                        </div>
+                    </div>            
+                </div> 
+            )}
+        </>
+    )
 }
 
 export default FormModal
