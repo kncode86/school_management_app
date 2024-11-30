5c4efd29ee4e86c6f9a3501ab4f18eb83601754a
kncode86 <kulcsar.norbert.86@gmail.com>
2024-11-30 13:51:18 +0000

    feat: create Form design

---
+++ src/components/forms/TeacherForm.tsx
@@ -0,0 +1,9 @@
+'use client'
+
+const TeacherForm = ({type, data}: {type: 'create' | 'update'; data?: any}) => {
+    return (
+        <form action="">input</form>
+    )
+}
+
+export default TeacherForm
