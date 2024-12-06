4928fb62e26604df7cd0aa2bceba4d4f4b8e67c9
kncode86 <kulcsar.norbert.86@gmail.com>
2024-12-06 10:07:37 +0000

    feat: fetch Class-data

--- src/app/(dashboard)/list/teachers/[id]/page.tsx
+++ src/app/(dashboard)/list/teachers/[id]/page.tsx
@@ -124,7 +124,7 @@ const SingleTeacherPage = () => {
                 <div className="bg-white p-4 rounded-md">
                     <h1 className="text-xl font-semibold">Shortcuts</h1>
                     <div className="mt-4 flex gap-4 flex-wrap text-xs text-gray-500">
-                        <Link className="p-3 rounded-md bg-customSkyLight" href="/" >Teacher&apos;s Classes</Link>
+                        <Link className="p-3 rounded-md bg-customSkyLight" href={`/list/classes?supervisorId=${"teacher2"}`} >Teacher&apos;s Classes</Link>
                         <Link className="p-3 rounded-md bg-customPurpleLight" href={`/list/students?teacherId=${"teacher2"}`} >Teacher&apos;s Students</Link>
                         <Link className="p-3 rounded-md bg-customYellowLight" href="/" >Teacher&apos;s Lessons</Link>
                         <Link className="p-3 rounded-md bg-pink-50" href="/" >Teacher&apos;s Exams</Link>
