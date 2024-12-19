2822152afa8049e3038c4109688b61a61c0545a0
kncode86 <kulcsar.norbert.86@gmail.com>
2024-12-19 20:44:21 +0000

    feat: fetch Announcement data

--- src/components/Announcements.tsx
+++ src/components/Announcements.tsx
@@ -1,49 +1,72 @@
-const Announcements = () => {
-    return (
-        
+import prisma from "@/lib/prisma"
+import { auth } from "@clerk/nextjs/server";
+import { Prisma } from "@prisma/client";
+
+const Announcements = async () => {
+
+    const {userId, sessionClaims} = await auth();
+    const role = (sessionClaims?.metadata as {role? : string})?.role;
+
+    const roleConditions = {
+        teacher: { lessons: {some: { teacherId: userId! }}},
+        student: { students: {some: { id: userId! }}},
+        parent: { lessons: {some: { parentId: userId! }}},
+    }
+
+    const data = await prisma.announcement.findMany({
+        take: 3,
+        orderBy:{date:"desc"},
+        where: {
+            ...(role !== "admin" && {
+                OR: 
+                [
+                    {classId: null},
+                    {class: roleConditions[role as keyof typeof roleConditions] || {}}
+                ] as Prisma.AnnouncementWhereInput[]  
+            })
+        }
+    });
+
+    return (  
         <div className="bg-white rounded-md p-4">
             <div className="flex items-center justify-between">
                 <h1 className="text-xl font-semibold">Announcements</h1>
                 <span className="text-xs text-gray-400">View All</span>
             </div>
             <div className="flex flex-col gap-4 mt-4">
-                <div className="bg-customSkyLight rounded-md p-4">
+                {data[0] && <div className="bg-customSkyLight rounded-md p-4">
                     <div className="flex items-center justify-between">
-                        <h2 className="font-medium">Lorem ipsum dolor sit</h2>
-                        <span className="text-xs text-gray-400 bg-white rounded-md p-1">2025-01-01</span>
+                        <h2 className="font-medium">{data[0].title}</h2>
+                        <span className="text-xs text-gray-400 bg-white rounded-md p-1">
+                            {new Intl.DateTimeFormat("hu-HU").format(data[0].date)}
+                        </span>
                     </div>
-                    <p className="text-sm text-gray-400 mt-1">
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean ante orci,
-                        tincidunt nec justo eget, bibendum convallis felis. 
-                    </p>
-                </div>
+                    <p className="text-sm text-gray-400 mt-1">{data[0].description}</p>
+                </div>}
             </div>
             <div className="flex flex-col gap-4 mt-4">
-                <div className="bg-customPurpleLight rounded-md p-4">
+                {data[1] && <div className="bg-customPurpleLight rounded-md p-4">
                     <div className="flex items-center justify-between">
-                        <h2 className="font-medium">Lorem ipsum dolor sit</h2>
-                        <span className="text-xs text-gray-400 bg-white rounded-md p-1">2025-01-01</span>
+                        <h2 className="font-medium">{data[1].title}</h2>
+                        <span className="text-xs text-gray-400 bg-white rounded-md p-1">
+                            {new Intl.DateTimeFormat("hu-HU").format(data[1].date)}
+                        </span>
                     </div>
-                    <p className="text-sm text-gray-400 mt-1">
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean ante orci,
-                        tincidunt nec justo eget, bibendum convallis felis. 
-                    </p>
-                </div>
+                    <p className="text-sm text-gray-400 mt-1">{data[1].description}</p>
+                </div>}
             </div>
             <div className="flex flex-col gap-4 mt-4">
-                <div className="bg-customYellowLight rounded-md p-4">
+                {data[2] && <div className="bg-customYellowLight rounded-md p-4">
                     <div className="flex items-center justify-between">
-                        <h2 className="font-medium">Lorem ipsum dolor sit</h2>
-                        <span className="text-xs text-gray-400 bg-white rounded-md p-1">2025-01-01</span>
+                        <h2 className="font-medium">{data[2].title}</h2>
+                        <span className="text-xs text-gray-400 bg-white rounded-md p-1">
+                            {new Intl.DateTimeFormat("hu-HU").format(data[2].date)}
+                        </span>
                     </div>
-                    <p className="text-sm text-gray-400 mt-1">
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean ante orci,
-                        tincidunt nec justo eget, bibendum convallis felis. 
-                    </p>
-                </div>
+                    <p className="text-sm text-gray-400 mt-1">{data[2].description}</p>
+                </div>}
             </div>                        
-        </div>
-          
+        </div>       
     )
 }
 
